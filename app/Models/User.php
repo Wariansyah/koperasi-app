adb9e667a005b93fed314bcf3787c360708bd1dd
ahmadais <mgondol86@gmail.com>
2023-07-24 11:14:54 +0700

    modul user

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,28 +15,26 @@ class User extends Authenticatable
 
     public $incrementing = false;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $guarded = [];
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'no_induk',
+        'alamat',
+        'telepon',
+        'jenkel',
+        'tgl_lahir',
+        'tmpt_lahir',
+    ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
