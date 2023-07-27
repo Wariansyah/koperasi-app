08c451e2f688d1c5c1ff73d48ea7be6fa2fe03c2
RosiMulyadi <rosymulyadi0@gmail.com>
2023-07-27 14:10:05 +0700

    Modul Role

--- app/Http/Controllers/KasController.php
+++ app/Http/Controllers/KasController.php
@@ -181,16 +181,27 @@ class KasController extends Controller
         ]);
     }
 
-    public function insertKasFromPreviousDay()
+    public function insertKasForNextDay()
     {
         // Get the accumulated value from the previous day's "kas" record
-        $previousDayKas = Kas::where('date', date('Y-m-d', strtotime('-1 day')))->first();
-        $accumulatedValue = $previousDayKas ? $previousDayKas->current_value : 0;
+        $previousDayKas = Kas::where('date', Carbon::yesterday())->latest()->first();
+        $accumulatedValue = $previousDayKas ? $previousDayKas->kas_akhir : 0;
 
-        // Insert the new record with the accumulated value for the current day
+        // Get the "kas_masuk" value for the current day (you may get it from the request or any other source)
+        $kasMasuk = 1000; // Replace this with the actual "kas_masuk" value for the current day
+
+        // Calculate the "kas_akhir" for the following day (which will be the "kas_awal" for the new record)
+        $kasAkhir = $accumulatedValue + $kasMasuk;
+
+        // Insert the new record with the calculated values for the current day
         Kas::create([
-            'date' => date('Y-m-d'),
-            'current_value' => $accumulatedValue,
+            'date' => Carbon::today()->addDay(), // The date for the following day
+            'kas_awal' => $kasAkhir,
+            'kas_masuk' => 0, // Set the "kas_masuk" for the new day to 0 initially
+            'kas_keluar' => 0, // Set the "kas_keluar" for the new day to 0 initially
+            'kas_akhir' => $kasAkhir,
+            'note' => 'Generated for the next day',
+            // Add any other fields you may have in the "kas" table
         ]);
 
         // Redirect back to the index page or perform any desired action
